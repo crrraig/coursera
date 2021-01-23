7f0680dfed0dcc1952d88536786f0874def510d1
crrraig <77246724+crrraig@users.noreply.github.com>
2021-01-23 18:27:43 -0500

    Delete mod4-solution directory

--- mod4-solution/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-var byespeaker = {};
-var speakword = "GoodBye";
-byespeaker.speak = function(names){
-  console.log(speakword + " " + names);
-}
-window.byespeaker = byespeaker;
-})(window);
--- mod4-solution/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-var hellospeaker = {};
-var speakword = "Hello";
-hellospeaker.speak = function(names){
-  console.log(speakword + " " + names);
-}
-window.hellospeaker = hellospeaker;
-})(window);
